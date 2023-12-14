ecb847d7a97d5288c2f771ffb7d4441b34ecfedb
Ahmed Charfeddine <60301833+ahmed-123-source@users.noreply.github.com>
2023-12-14 17:50:15 +0100

    Delete UserService directory

--- UserService/src/main/java/tn/esprit/spring/ForgotPassword/generateToen.java
+++
@@ -1,4 +0,0 @@
-package tn.esprit.spring.ForgotPassword;
-
-public class generateToen {
-}
--- UserService/src/main/java/tn/esprit/spring/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginDto {
-    private String userName;
-    private String password;
-}
